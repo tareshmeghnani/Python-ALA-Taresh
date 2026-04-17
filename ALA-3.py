ce74dca24a140d7932340cf7dfb0ed55c4d24bd9
tareshmeghnani <tareshsureshbhai@gmail.com>
2026-04-18 03:21:01 +0530

    Add files via upload

---
+++ ALA-3.py
@@ -0,0 +1,14 @@
+""" #include<stdio.h>
+int main(){
+    for(int i = 1; i <= 10; i++){
+        printf("%d\n", i);
+    }
+    return 0;
+}"""
+
+#Given Code is in C language converting to python programmimg language 
+
+for i in range(1, 11):           # for i in range(1, 11) → runs from 1 to 10 (since 11 is excluded)
+    print(i)                   # print(i) → directly prints the value
+
+
